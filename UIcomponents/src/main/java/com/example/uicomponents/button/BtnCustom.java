9f4848c20fb25e44d76a92886083af5ccbf99471
student-a502 <student-a502@STUD-RM3-A502>
2026-04-28 15:48:03 +0500

    сделано до 15го шага Сержанини

---
+++ UIcomponents/src/main/java/com/example/uicomponents/button/BtnCustom.java
@@ -0,0 +1,11 @@
+package com.example.uicomponents.button;
+
+import android.widget.Button;
+
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+public class BtnCustom extends ConstraintLayout {
+    public Button Btn;
+
+    public enum T
+}
